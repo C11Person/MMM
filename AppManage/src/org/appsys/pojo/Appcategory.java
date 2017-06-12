6202e0a034f1ecc78267aa1a274f467afec0a85c
lengmo <1026775779@qq.com>
2017-06-12 12:14:37 +0800

    添加APP信息管理平台

---
+++ AppManage/src/org/appsys/pojo/Appcategory.java
@@ -0,0 +1,65 @@
+package org.appsys.pojo;
+
+import java.util.Date;
+
+public class Appcategory {
+	private int id;
+	private String categoryCode;//分类编码
+	private String categoryName;//分类名称
+	private int parentId;//父级ID
+	private int createdBy;
+	private Date creationTime;
+	private int modifyBy;
+	private Date modifyDate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCategoryCode() {
+		return categoryCode;
+	}
+	public void setCategoryCode(String categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public int getParentId() {
+		return parentId;
+	}
+	public void setParentId(int parentId) {
+		this.parentId = parentId;
+	}
+	public int getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(int createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreationTime() {
+		return creationTime;
+	}
+	public void setCreationTime(Date creationTime) {
+		this.creationTime = creationTime;
+	}
+	public int getModifyBy() {
+		return modifyBy;
+	}
+	public void setModifyBy(int modifyBy) {
+		this.modifyBy = modifyBy;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	
+	
+	
+}
