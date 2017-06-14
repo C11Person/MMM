b0c45451533c6841cf7ac3f4c812ff7308d56f5c
lengmo <1026775779@qq.com>
2017-06-14 17:16:36 +0800

    添加项目

---
+++ AppManage/src/org/appsys/controller/DevUserController.java
@@ -0,0 +1,269 @@
+package org.appsys.controller;
+
+import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.math.RandomUtils;
+import org.apache.ibatis.annotations.Param;
+import org.apache.log4j.Logger;
+import org.appsys.pojo.AppCategory;
+import org.appsys.pojo.AppInfo;
+import org.appsys.pojo.DataDictionary;
+import org.appsys.pojo.DevUser;
+import org.appsys.service.DevUserService;
+import org.appsys.tool.Constans;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+
+@Controller
+public class DevUserController {
+	private Logger logger =Logger.getLogger("DevUserController.class");
+	@Autowired
+	DevUserService devUserService;
+
+	@RequestMapping(value = "/login.html")
+	public String lo() {
+		return "devlogin";
+	}
+
+	@RequestMapping(value = "/dologin.html", method = RequestMethod.POST)
+	public String login(String devCode, String devPassword, HttpSession session) {
+		/*
+		 * ApplicationContext context = new
+		 * ClassPathXmlApplicationContext("SpringConfig.xml"); DevUserService
+		 * devUserService = (DevUserService) context.getBean("devUserService");
+		 */
+		DevUser devUser = devUserService.selectUserByNameAndPwd(devCode,
+				devPassword);
+		if(devUser != null){
+			session.setAttribute("devUserSession", devUser);
+			return "developer/main";
+		}else{
+			session.setAttribute("error", "用户名或密码错误");
+			return "devlogin";
+		}
+	}
+
+	@RequestMapping(value = "/dev/logout.html")
+	public String loginout(HttpSession session) {
+		session.invalidate();
+		return "devlogin";
+	}
+
+	// APP维护
+	@RequestMapping(value = "/list.html")
+	public String list(HttpSession session,
+			@RequestParam(required = false) String querySoftwareName,
+			@RequestParam(required = false) String queryStatus,
+			@RequestParam(required = false) String queryFlatformId,
+			@RequestParam(required = false) String queryCategoryLevel1,
+			@RequestParam(required = false) String queryCategoryLevel2,
+			@RequestParam(required = false) String queryCategoryLevel3,
+			@RequestParam(required = false) String pageIndex) {
+		
+		
+		if (pageIndex == null) {
+			pageIndex = "1";
+		}
+		if (queryStatus == null || queryStatus.equals("")) {
+			queryStatus = "0";
+		}
+		if (queryFlatformId == null || queryFlatformId.equals("")) {
+			queryFlatformId = "0";
+		}
+		if (queryCategoryLevel1 == null || queryCategoryLevel1.equals("")) {
+			queryCategoryLevel1 = "0";
+		}
+		if (queryCategoryLevel2 == null || queryCategoryLevel2.equals("")) {
+			queryCategoryLevel2 = "0";
+		}
+		if (queryCategoryLevel3 == null || queryCategoryLevel3.equals("")) {
+			queryCategoryLevel3 = "0";
+		}
+
+		int count = devUserService.selectAppCount(querySoftwareName,
+				Integer.parseInt(queryStatus),
+				Integer.parseInt(queryFlatformId),
+				Integer.parseInt(queryCategoryLevel1),
+				Integer.parseInt(queryCategoryLevel2),
+				Integer.parseInt(queryCategoryLevel3));
+		int pageCount = count % Constans.PAGE_SIZE == 0 ? count
+				/ Constans.PAGE_SIZE : count / Constans.PAGE_SIZE + 1;
+		System.out.println("================" + count);
+		Map map = new HashMap();
+		map.put("totalCount", count);
+		map.put("totalPageCount", pageCount);
+		map.put("currentPageNo", pageIndex);
+		session.setAttribute("pages", map);
+		// 显示所有
+		List<AppInfo> appInfoList = devUserService.appList(querySoftwareName,
+				Integer.parseInt(queryStatus),
+				Integer.parseInt(queryFlatformId),
+				Integer.parseInt(queryCategoryLevel1),
+				Integer.parseInt(queryCategoryLevel2),
+				Integer.parseInt(queryCategoryLevel3),
+				Integer.parseInt(pageIndex), Constans.PAGE_SIZE);
+
+		// app状态
+		List<DataDictionary> statusList = devUserService.statusList();
+
+		// 所属平台
+		List<DataDictionary> flatFormList = devUserService.flatFormList();
+
+		// 一级分类
+		List<AppCategory> categoryLevel1List = devUserService
+				.categoryLevel1List();
+		session.setAttribute("appInfoList", appInfoList);
+		session.setAttribute("statusList", statusList);
+		session.setAttribute("flatFormList", flatFormList);
+		session.setAttribute("categoryLevel1List", categoryLevel1List);
+		return "developer/appinfolist";
+	}
+
+	@RequestMapping(value = "/categorylevel2list", produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public String li2(String pid) {
+		List<AppCategory> categoryLevel2List = devUserService.categoryLevel2List(Integer.parseInt(pid));
+		return JSONArray.toJSONString(categoryLevel2List);
+	}
+
+	@RequestMapping(value = "/appinfoadd.html")
+	public String appinfoadd(){
+		return "developer/appinfoadd";
+	}
+	
+	
+	@RequestMapping(value = "/datadictionarylist", produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public String datadictionarylist() {
+		List<DataDictionary> datadictionarylist = devUserService.flatFormList();
+		return JSONArray.toJSONString(datadictionarylist);
+	}
+	
+	@RequestMapping(value = "/categoryLevel1List", produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public String categoryLevel1List() {
+		List<AppCategory> categoryLevel1List = devUserService.categoryLevel1List();
+		return JSONArray.toJSONString(categoryLevel1List);
+	}
+	
+	@RequestMapping(value = "/apkexist", produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public String selectDevByName(String APKName) {
+		AppInfo appInfo= devUserService.selectDevByName(APKName);
+		HashMap<String,String> resutlt = new HashMap<String, String>();
+		if(APKName.isEmpty()){
+			resutlt.put("APKName","empty");
+		}else if(appInfo!=null){
+			resutlt.put("APKName","exist");
+		}else if(appInfo==null){
+			resutlt.put("APKName","noexist");
+		}
+		
+		return JSONArray.toJSONString(resutlt);
+	}
+	
+	
+	//添加
+	@RequestMapping(value="/appinfoaddsave.html",method=RequestMethod.POST)
+	public String useradd( AppInfo appInfo,HttpSession session,HttpServletRequest request,
+			 @RequestParam(value ="a_logoPicPath", required = false) MultipartFile attach){
+		String hid_logoPicPath = request.getParameter("hid_logoPicPath");
+		System.out.println(hid_logoPicPath);
+/*			@Valid
+ * 			BindingResult bindingResult
+ * 			if(bindingResult.hasErrors()){  
+			return "developer/appinfoadd";			
+		}*/
+		String logoLocPath = null;
+		//判断文件是否为空
+		if(!attach.isEmpty()){
+			String path = request.getSession().getServletContext().getRealPath("statics"+File.separator+"uploadfiles");			
+			String oldFileName = attach.getOriginalFilename();//原文件名		
+			String prefix=FilenameUtils.getExtension(oldFileName);//原文件后缀   
+			int filesize = 512000;
+			
+	        if(attach.getSize() >  filesize){//上传大小不得超过 500k
+           	request.setAttribute("uploadFileError", " * 上传大小不得超过 500k");
+	        	return "useradd";
+           }else if(prefix.equalsIgnoreCase("jpg") || prefix.equalsIgnoreCase("png") 
+           		|| prefix.equalsIgnoreCase("jpeg") || prefix.equalsIgnoreCase("pneg")){//上传图片格式不正确
+           	String fileName = System.currentTimeMillis()+RandomUtils.nextInt(1000000)+"_Personal.jpg";  
+               logger.debug("new fileName======== " + attach.getName());
+               File targetFile = new File(path, fileName);  
+               if(!targetFile.exists()){  
+                   targetFile.mkdirs();  
+               }  
+               //保存  
+               try {  
+               	attach.transferTo(targetFile);  
+               } catch (Exception e) {  
+                   e.printStackTrace();  
+                   request.setAttribute("uploadFileError", " * 上传失败！");
+                   return "developer/appinfoadd";
+               }  
+               logoLocPath = path+File.separator+fileName;
+           }else{
+           	request.setAttribute("uploadFileError", " * 上传图片格式不正确");
+           	return "developer/appinfoadd";
+           }
+		}
+		int devId=((DevUser)session.getAttribute("devUserSession")).getId();
+		appInfo.setDevId(devId);
+		appInfo.setCreatedBy(devId);
+		appInfo.setCreationDate(new Date());
+		appInfo.setStatus(1);
+/*		hid_logoPicPath=request.getParameter("hid_logoPicPath");
+		System.out.println("==========================================================="+hid_logoPicPath);*/
+		appInfo.setLogoPicPath(hid_logoPicPath);
+		appInfo.setLogoLocPath(logoLocPath);
+		boolean flag = devUserService.addAppInfo(appInfo);
+		if(flag){
+			System.out.println("添加成功！");
+			return "redirect:/list.html";
+		}else{
+			System.out.println("添加失败！");
+			return "false";
+		}
+	}
+	
+	
+	//修改
+	@RequestMapping(value="/appinfomodify/{appinfoid}")
+	public String updateAppinfo(@PathVariable int appinfoid,HttpSession session){
+		
+		AppInfo appInfo=devUserService.selectAppById(appinfoid);
+		session.setAttribute("appInfo", appInfo);
+		return "developer/appinfomodify";
+	}
+	
+	
+	@RequestMapping(value = "/categoryLevelAll", produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public String categoryLevelAll() {
+		List<AppCategory> categoryLevel1List = devUserService.categoryLevel1List();
+		return JSONArray.toJSONString(categoryLevel1List);
+	}
+	
+	
+}
---
+++ AppManage/src/org/appsys/pojo/AppInfo.java
@@ -0,0 +1,230 @@
+package org.appsys.pojo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class AppInfo {
+	private int id;
+	
+	@NotEmpty(message="软件名称不能为空")
+	private String softwareName;//软件名称
+	private String APKName;//apk名称
+	@NotEmpty(message="支持的ROM不能为空")
+	private String supportROM;//支持ROM
+	@NotEmpty(message="界面语言不能为空")
+	private String interfaceLanguage;//界面语言
+	@NotEmpty(message="软件大小不能为空")
+	private BigDecimal softwareSize;//软件大小
+	private Date updateDate;//更新日期
+	private int devId;//开发者ID
+	@NotEmpty(message="应用简介不能为空")
+	private String appInfo;//应用简介
+	private int status;//状态 1待审核 2审核通过 3 审核不通过 4 已上架 5 已下架 
+	private Date onSaleDate;//上架时间
+	private Date offSaleDate;//下架时间
+	private int flatformId;//所属平台
+	private int categoryLevel3;//所属三级分类
+	@NotEmpty(message="下载量不能为空")
+	private int downloads;//下载量
+	private int createdBy;
+	private Date creationDate;
+	private int modifyBy;
+	private Date modifyDate;
+	private int categoryLevel1;//所属一级分类
+	private int categoryLevel2;//所属二级分类
+	private String logoPicPath;//LOGO图片url路径
+	private String logoLocPath;//LOGO图片的服务器存储路径
+	public String getLogoLocPath() {
+		return logoLocPath;
+	}
+	public void setLogoLocPath(String logoLocPath) {
+		this.logoLocPath = logoLocPath;
+	}
+	private int versionId;//最新的版本id
+	
+	private String flatformName;
+	private String categoryLevel1Name;
+	private String categoryLevel2Name;
+	private String categoryLevel3Name;
+	private String versionNo;
+	
+	public String getVersionNo() {
+		return versionNo;
+	}
+	public void setVersionNo(String versionNo) {
+		this.versionNo = versionNo;
+	}
+	public String getFlatformName() {
+		return flatformName;
+	}
+	public void setFlatformName(String flatformName) {
+		this.flatformName = flatformName;
+	}
+	public String getCategoryLevel1Name() {
+		return categoryLevel1Name;
+	}
+	public void setCategoryLevel1Name(String categoryLevel1Name) {
+		this.categoryLevel1Name = categoryLevel1Name;
+	}
+	public String getCategoryLevel2Name() {
+		return categoryLevel2Name;
+	}
+	public void setCategoryLevel2Name(String categoryLevel2Name) {
+		this.categoryLevel2Name = categoryLevel2Name;
+	}
+	public String getCategoryLevel3Name() {
+		return categoryLevel3Name;
+	}
+	public void setCategoryLevel3Name(String categoryLevel3Name) {
+		this.categoryLevel3Name = categoryLevel3Name;
+	}
+	public String getStatusName() {
+		return statusName;
+	}
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+	private String statusName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSoftwareName() {
+		return softwareName;
+	}
+	public void setSoftwareName(String softwareName) {
+		this.softwareName = softwareName;
+	}
+	public String getAPKName() {
+		return APKName;
+	}
+	public void setAPKName(String aPKName) {
+		APKName = aPKName;
+	}
+	public String getSupportROM() {
+		return supportROM;
+	}
+	public void setSupportROM(String supportROM) {
+		this.supportROM = supportROM;
+	}
+	public String getInterfaceLanguage() {
+		return interfaceLanguage;
+	}
+	public void setInterfaceLanguage(String interfaceLanguage) {
+		this.interfaceLanguage = interfaceLanguage;
+	}
+	public BigDecimal getSoftwareSize() {
+		return softwareSize;
+	}
+	public void setSoftwareSize(BigDecimal softwareSize) {
+		this.softwareSize = softwareSize;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	public int getDevId() {
+		return devId;
+	}
+	public void setDevId(int devId) {
+		this.devId = devId;
+	}
+	public String getAppInfo() {
+		return appInfo;
+	}
+	public void setAppInfo(String appInfo) {
+		this.appInfo = appInfo;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Date getOnSaleDate() {
+		return onSaleDate;
+	}
+	public void setOnSaleDate(Date onSaleDate) {
+		this.onSaleDate = onSaleDate;
+	}
+	public Date getOffSaleDate() {
+		return offSaleDate;
+	}
+	public void setOffSaleDate(Date offSaleDate) {
+		this.offSaleDate = offSaleDate;
+	}
+	public int getFlatformId() {
+		return flatformId;
+	}
+	public void setFlatformId(int flatformId) {
+		this.flatformId = flatformId;
+	}
+	public int getCategoryLevel3() {
+		return categoryLevel3;
+	}
+	public void setCategoryLevel3(int categoryLevel3) {
+		this.categoryLevel3 = categoryLevel3;
+	}
+	public int getDownloads() {
+		return downloads;
+	}
+	public void setDownloads(int downloads) {
+		this.downloads = downloads;
+	}
+	public int getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(int createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public int getModifyBy() {
+		return modifyBy;
+	}
+	public void setModifyBy(int modifyBy) {
+		this.modifyBy = modifyBy;
+	}
+	public Date getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(Date modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public int getCategoryLevel1() {
+		return categoryLevel1;
+	}
+	public void setCategoryLevel1(int categoryLevel1) {
+		this.categoryLevel1 = categoryLevel1;
+	}
+	public int getCategoryLevel2() {
+		return categoryLevel2;
+	}
+	public void setCategoryLevel2(int categoryLevel2) {
+		this.categoryLevel2 = categoryLevel2;
+	}
+	public String getLogoPicPath() {
+		return logoPicPath;
+	}
+	public void setLogoPicPath(String logoPicPath) {
+		this.logoPicPath = logoPicPath;
+	}
+
+	public int getVersionId() {
+		return versionId;
+	}
+	public void setVersionId(int versionId) {
+		this.versionId = versionId;
+	}
+}
